d898319052294281c1af207038d28958a6341c61
dlwortmann <dannywortmann@gmail.com>
2025-04-01 16:16:29 -0500

    added another way of connecting to DB_URL and but not necessary

--- Develop/server/src/models/index.ts
+++ Develop/server/src/models/index.ts
@@ -5,6 +5,21 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
+// let sequelize;
+// if (process.env.DB_URL) {
+//   sequelize = new Sequelize(process.env.DB_URL);
+// } else {
+//   sequelize = new Sequelize (
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//     }
+//   )
+// }
+
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
