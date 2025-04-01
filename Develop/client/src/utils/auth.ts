0b8a7bd3936327c33ebc13931ae09ebde893bbf0
dlwortmann <dannywortmann@gmail.com>
2025-04-01 12:06:44 -0500

    get install complete and add node modules to server folder

--- Develop/client/src/utils/auth.ts
+++ Develop/client/src/utils/auth.ts
@@ -3,10 +3,13 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    const token = this.getToken();
+    return token;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    
   }
   
   isTokenExpired(token: string) {
@@ -15,16 +18,22 @@ class AuthService {
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token') || '';
+    return loggedUser
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
     // TODO: redirect to the home page
+    localStorage.setItem('id_token', idToken)
+    window.location.assign('/')
   }
 
   logout() {
     // TODO: remove the token from localStorage
     // TODO: redirect to the login page
+    localStorage.removeItem("id_token")
+    window.location.assign('/')
   }
 }
 
--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -5,6 +5,22 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+  const user = await User.findOne({
+    where: { username }
+  });
+  if (!user) {
+    return res.status(401).json({ message: "Failed to authenticate."})
+  }
+
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) {
+    return res.status(401).json({ message: "Failed to authenticate."})
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+  const token = jwt.sign({ username }, secretKey, { expiresIn: '1h' });
+  return res.json({ token });
 };
 
 const router = Router();
